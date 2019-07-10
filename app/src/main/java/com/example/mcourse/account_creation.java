67cf264aadb92a2b9b887337be06c5ae0106a33f
Arjun <arjunsundram@gmail.com>
2019-07-10 13:49:37 +1000

    Managed to upload a bit of the profile details to the Firebase Database.

--- app/src/main/java/com/example/mcourse/account_creation.java
+++ app/src/main/java/com/example/mcourse/account_creation.java
@@ -17,32 +17,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 public class account_creation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button create_profile_button;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_creation);
         final EditText fname_input = findViewById(R.id.fname_input);
         final EditText lname_input = findViewById(R.id.lname_input);
         final EditText email_input = findViewById(R.id.email_input);
         final EditText phone_number_input = findViewById(R.id.phone_number_input);
         final EditText dob_input = findViewById(R.id.dob_input);
         final EditText password_input = findViewById(R.id.password_input);
-        final Button create_profile_button = findViewById(R.id.create_profile_button);
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_creation);
+        create_profile_button = (Button) findViewById(R.id.create_profile_button);
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Log.d("arjun", "REACHED THE CREATE PROFILE POINT");
 
         create_profile_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Map<String, Object> user = new HashMap<>();
-                user.put("f_name", fname_input.getText());
-                user.put("l_name", lname_input.getText());
-                user.put("email", email_input.getText());
-                user.put("phone_number", phone_number_input.getText());
-                user.put("dob", dob_input.getText());
-                user.put("password", password_input.getText());
+                user.put("f_name", fname_input.getText().toString());
+                user.put("l_name", lname_input.getText().toString());
+                user.put("email", email_input.getText().toString());
+                user.put("phone_number", phone_number_input.getText().toString());
+                user.put("dob", dob_input.getText().toString());
+                user.put("password", password_input.getText().toString());
                 user.put("finished_creation", false);
+
+
 
                 db.collection("users")
                         .add(user)
