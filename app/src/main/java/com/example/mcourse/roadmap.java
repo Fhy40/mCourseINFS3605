44f7ceafecb690ab42945abdab7855679369278a
liangshuhao123 <251793860@qq.com>
2019-07-21 18:34:22 +1000

    button style xml fixed up

--- app/src/main/java/com/example/mcourse/roadmap.java
+++ app/src/main/java/com/example/mcourse/roadmap.java
@@ -3,11 +3,59 @@ package com.example.mcourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class roadmap extends AppCompatActivity {
 
+    
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ScrollView scrollView = findViewById(R.id.scrollView);
+
+        Button btn_y3_t3 = findViewById(R.id.button_y3_t3);
+        Button btn_y3_t2 = findViewById(R.id.button_y3_t2);
+        Button btn_y3_t1 = findViewById(R.id.button_y3_t1);
+        Button btn_y2_t3 = findViewById(R.id.button_y2_t3);
+        Button btn_y2_t2 = findViewById(R.id.button_y2_t2);
+        Button btn_y2_t1 = findViewById(R.id.button_y2_t1);
+        Button btn_y1_t3 = findViewById(R.id.button_y1_t3);
+        Button btn_y1_t2 = findViewById(R.id.button_y1_t2);
+        Button btn_y1_t1 = findViewById(R.id.button_y1_t1);
+
+        Button btn_t9_1 = findViewById(R.id.t9_1);
+        Button btn_t9_2 = findViewById(R.id.t9_2);
+        Button btn_t9_3 = findViewById(R.id.t9_3);
+        Button btn_t8_1 = findViewById(R.id.t8_1);
+        Button btn_t8_2 = findViewById(R.id.t8_2);
+        Button btn_t8_3 = findViewById(R.id.t8_3);
+        Button btn_t7_1 = findViewById(R.id.t7_1);
+        Button btn_t7_2 = findViewById(R.id.t7_2);
+        Button btn_t7_3 = findViewById(R.id.t7_3);
+
+        Button btn_t6_1 = findViewById(R.id.t6_1);
+        Button btn_t6_2 = findViewById(R.id.t6_2);
+        Button btn_t6_3 = findViewById(R.id.t6_3);
+        Button btn_t5_1 = findViewById(R.id.t5_1);
+        Button btn_t5_2 = findViewById(R.id.t5_2);
+        Button btn_t5_3 = findViewById(R.id.t5_3);
+        Button btn_t4_1 = findViewById(R.id.t4_1);
+        Button btn_t4_2 = findViewById(R.id.t4_2);
+        Button btn_t4_3 = findViewById(R.id.t4_3);
+
+        Button btn_t3_1 = findViewById(R.id.t3_1);
+        Button btn_t3_2 = findViewById(R.id.t3_2);
+        Button btn_t3_3 = findViewById(R.id.t3_3);
+        Button btn_t2_1 = findViewById(R.id.t2_1);
+        Button btn_t2_2 = findViewById(R.id.t2_2);
+        Button btn_t2_3 = findViewById(R.id.t2_3);
+        Button btn_t1_1 = findViewById(R.id.t1_1);
+        Button btn_t1_2 = findViewById(R.id.t1_2);
+        Button btn_t1_3 = findViewById(R.id.t1_3);
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roadmap);
 
